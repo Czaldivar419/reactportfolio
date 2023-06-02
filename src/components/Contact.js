f465181ba21ff91f613b9efeeecfbdba57ee341a
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2023-06-02 04:16:59 -0700

    Removed server file, completely front end

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,47 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/media/contactImg.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
 
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
+  const form = useRef();
+  const [showMessageSent, setShowMessageSent] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code === 200) {
-      setStatus({ success: true, message: 'Thanks for reaching out!'});
-    } else {
-      setStatus({ success: false, message: 'Something went wrong, please try again later.'});
-    }
-  };
+
+    emailjs.sendForm("service_xtz6eu8", "template_uw2s2eq", form.current, "AiGO8oL6SL_yM2GhI")
+    .then((result) => {
+      console.log(result.text);
+      setShowMessageSent(true);
+      form.current.reset();
+      setTimeout(() => setShowMessageSent(false), 3000); 
+  }, (error) => {
+      console.log(error.text);
+  });
+};
 
   return (
     <section className="contact" id="connect">
@@ -59,32 +41,18 @@ const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    }
-                  </Row>
-                </form>
+                <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Phone Number</label>
+                <input type="tel" name="phone_number" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+                {showMessageSent && <div>Message sent!</div>}
+              </form>
               </div>}
             </TrackVisibility>
           </Col>
